7f09c3cd6d14086b90db9e93424f60c795d88f7c
BOUDAR Ahmed <37126117+boudarahmed@users.noreply.github.com>
2021-05-25 13:28:06 +0200

    Add files via upload

---
+++ CGU.php
@@ -0,0 +1,80 @@
+<?php $current_page = "CGU"; ?>
+<?php require_once("./includes/header.php"); ?>
+<div id="layoutDefault">
+ <div id="layoutDefault_content">
+  <main>
+
+   <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
+    <div class="container">
+     <a class="navbar-brand text-dark" href="index.php">AMDI BLOG</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mr-lg-5">
+       <!-- <li class="nav-item">
+        <a class="nav-link" href="index.php">Home </a>
+       </li> -->
+       <li class="nav-item dropdown no-caret">
+        <a class="nav-link" href="contact.php">Contact</a>
+       </li>
+       <li class="nav-item dropdown no-caret">
+        <a class="nav-link" href="about.php">Qui sommes-nous ?</a>
+       </li>
+      </ul>
+      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signin.php">Sign in<i class="fas fa-arrow-right ml-1"></i></a>
+      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signup.php">Sign up<i class="fas fa-arrow-right ml-1"></i></a>
+     </div>
+    </div>
+   </nav>
+
+   <header class="page-header page-header-dark bg-gradient-primary-to-secondary">
+    <div class="page-header-content pt-10">
+     <div class="container text-center">
+      <div class="row justify-content-center">
+       <div class="col-lg-8">
+        <h1 class="page-header-title mb-3">CGU</h1>
+       </div>
+      </div>
+     </div>
+    </div>
+    <div class="svg-border-rounded text-white">
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
+      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
+    </div>
+   </header>
+   <section class="bg-white py-10">
+    <div class="container">
+     <div>
+      <h1>Conditions générales d'utilisation du blog</h1>
+      <p class="lead">Propriété intellectuelle</p>
+      <p>Les marques, logos, signes et tout autre contenu du site font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur.</p>
+      <p>L'Utilisateur sollicite l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus.</p>
+      <p>L'Utilisateur s'engage à une utilisation des contenus du site dans un cadre strictement privé. Une utilisation des contenus à des fins commerciales est strictement interdite.</p>
+      <p>Tout contenu mis en ligne par l'Utilisateur est de sa seule responsabilité. L'Utilisateur s'engage à ne pas mettre en ligne de contenus pouvant porter atteinte aux intérêts de tierces personnes. Tout recours en justice engagé par un tiers lésé contre le site sera pris en charge par l'Utilisateur.</p>
+      <p>Le contenu de l'Utilisateur peut être à tout moment et pour n'importe quelle raison supprimé ou modifié par le site. L'Utilisateur ne reçoit aucune justification et notification préalablement à la suppression ou à la modification du contenu Utilisateur.</p>
+     </div>
+
+    </div>
+
+    <div class="svg-border-rounded text-dark">
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
+      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
+    </div>
+   </section>
+  </main>
+ </div>
+ <div id="layoutDefault_footer">
+  <footer class="footer pt-4 pb-4 mt-auto bg-dark footer-dark">
+   <div class="container">
+    <hr class="my-1" />
+    <div class="row align-items-center">
+     <div class="col-md-6 small">Copyright &#xA9; AMDI4 2021</div>
+     <div class="col-md-6 text-md-right small">
+      <a href="privacy-policy.php">Mentions légales</a>
+      &#xB7;
+      <a href="CGU.php">CGU</a>
+     </div>
+    </div>
+   </div>
+  </footer>
+ </div>
+</div>
+<?php require_once("./includes/footer.php"); ?>
---
+++ privacy-policy.php
@@ -0,0 +1,78 @@
+<?php $current_page = "Mentions légales"; ?>
+<?php require_once("./includes/header.php"); ?>
+<div id="layoutDefault">
+ <div id="layoutDefault_content">
+  <main>
+
+   <nav class="navbar navbar-marketing navbar-expand-lg bg-white navbar-light">
+    <div class="container">
+     <a class="navbar-brand text-dark" href="index.php">AMDI BLOG</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><img src="img/menu.png" style="height:20px;width:25px" /><i data-feather="menu"></i></button>
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mr-lg-5">
+       <!-- <li class="nav-item">
+        <a class="nav-link" href="index.php">Home </a>
+       </li> -->
+       <li class="nav-item dropdown no-caret">
+        <a class="nav-link" href="contact.php">Contact</a>
+       </li>
+       <li class="nav-item dropdown no-caret">
+        <a class="nav-link" href="about.php">Qui sommes-nous ?</a>
+       </li>
+      </ul>
+      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signin.php">Sign in<i class="fas fa-arrow-right ml-1"></i></a>
+      <a class="btn-teal btn rounded-pill px-4 ml-lg-4" href="backend/signup.php">Sign up<i class="fas fa-arrow-right ml-1"></i></a>
+     </div>
+    </div>
+   </nav>
+
+   <header class="page-header page-header-dark bg-gradient-primary-to-secondary">
+    <div class="page-header-content pt-10">
+     <div class="container text-center">
+      <div class="row justify-content-center">
+       <div class="col-lg-8">
+        <h1 class="page-header-title mb-3">Mentions légales</h1>
+       </div>
+      </div>
+     </div>
+    </div>
+    <div class="svg-border-rounded text-white">
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
+      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
+    </div>
+   </header>
+   <section class="bg-white py-10">
+    <div class="container">
+     <div>
+      <h1>Mentions légales</h1>
+      <p class="lead">Propriété intellectuelle et contrefaçons</p>
+      <p>Le groupe AMDI4 est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logo, icônes, sons, logiciels.</p>
+      <p>Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable du groupe AMDI4.</p>
+      <p>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+     </div>
+
+    </div>
+
+    <div class="svg-border-rounded text-dark">
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
+      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
+    </div>
+   </section>
+  </main>
+ </div>
+ <div id="layoutDefault_footer">
+  <footer class="footer pt-4 pb-4 mt-auto bg-dark footer-dark">
+   <div class="container">
+    <hr class="my-1" />
+    <div class="row align-items-center">
+     <div class="col-md-6 small">Copyright &#xA9; AMDI4 2021</div>
+     <div class="col-md-6 text-md-right small">
+      <a href="privacy-policy.php">Mentions légales</a>
+      &#xB7;
+      <a href="CGU.php">CGU</a>
+     </div>
+    </div>
+   </div>
+  </footer>
+ </div>
+</div>
+<?php require_once("./includes/footer.php"); ?>
